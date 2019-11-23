8465a521b9d5dde41e3439ffc42e84230ab19cb8
SoraKumo <info@croud.jp>
2019-11-23 13:45:39 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import * as fs from "fs";
+try {
+  const ref = process.env.GITHUB_REF;
+  if (!ref) throw "Error: 'refs/tags' not found";
+
+  const fileName = "package.json";
+  const packageValues = JSON.parse(fs.readFileSync(fileName).toString());
+  const v = ref.match(/^refs\/tags\/(.*?)-(.*?)$/);
+  if (!v || v.length < 3) throw "Error: 'refs/tags' format error";
+  const version = v[2];
+  console.log("version: %s", version);
+  fs.writeFileSync(
+    fileName,
+    JSON.stringify({ ...packageValues, version }, undefined, "  ")
+  );
+} catch (e) {
+  console.error(e);
+  process.exit(-1);
+}
